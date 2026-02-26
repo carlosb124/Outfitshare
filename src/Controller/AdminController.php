215d10649da83101d2ff09d1c2fd2c4f05606886
Jeandeivid <jeazaprom2@alu.edu.gva.es>
2026-02-26 17:08:11 +0100

    fix: extend EasyAdmin DashboardController to fix i18n null variable error

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,24 +4,41 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\Repository\OutfitRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[IsGranted('ROLE_ADMIN')]
-class AdminController extends AbstractController
+class AdminController extends AbstractDashboardController
 {
+    public function __construct(
+        private UserRepository $userRepository,
+        private OutfitRepository $outfitRepository,
+    ) {}
+
     #[Route('/admin', name: 'app_admin_dashboard')]
-    public function index(UserRepository $userRepository, OutfitRepository $outfitRepository): Response
+    public function index(): Response
     {
-        // Get total counts for the dashboard
-        $countUsers = $userRepository->count([]);
-        $countOutfits = $outfitRepository->count([]);
-        
+        $countUsers = $this->userRepository->count([]);
+        $countOutfits = $this->outfitRepository->count([]);
+
         return $this->render('admin/dashboard.html.twig', [
             'count_users' => $countUsers,
             'count_outfits' => $countOutfits,
         ]);
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('OutfitShare Admin');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
     }
 }
